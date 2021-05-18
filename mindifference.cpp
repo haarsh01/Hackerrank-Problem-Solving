f6cd5762d9b912766d44157c469cee416c3da3d8
Harsh Kaushik <60865477+haarsh01@users.noreply.github.com>
2021-05-18 22:33:54 +0530

    Add files via upload

---
+++ mindifference.cpp
@@ -0,0 +1,14 @@
+int minimumAbsoluteDifference(vector<int> arr) {
+sort(arr.begin(), arr.end());
+int diff = abs(arr[0] - arr[1]);
+
+for(int i=arr.size() - 1;i>=0;i--){
+    if(abs(arr[i] - arr[i-1]) < diff)
+    diff = abs(arr[i] - arr[i-1]);
+}
+return diff;
+
+
+
+
+}
